ad65316ece5f1e9417a202727c24d88a7e7007f3
syinyichen <e0313651@u.nus.edu>
2020-01-19 20:44:38 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,6 +8,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         String line = "____________________________________________________________";
+        ArrayList<String> tasks = new ArrayList<String>();
         Scanner sc = new Scanner(System.in);
 
         //Greeting
@@ -18,8 +19,16 @@ public class Duke {
             if (input.equals("bye")) {
                 System.out.println(line + "\nBye. Hope to see you again soon!\n" + line);
                 break;
+            } else if (input.equals("list")) {
+                System.out.println(line);
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(i + 1 + ". " + tasks.get(i));
+                }
+                System.out.println(line);
+            } else {
+                tasks.add(input);
+                System.out.println(line + "\n" + "added: " + input + "\n" + line);
             }
-            System.out.println(line + "\n" + input + "\n" + line);
         }
     }
 }
