d25d7b3f154651c3a9c944b1279b001ce457eb0f
syinyichen <e0313651@u.nus.edu>
2020-01-20 22:28:12 +0800

    Revert "Level-6"
    This reverts commit e60de79fcadb8a27aa9ef2e3c5b672ea6ef9cd84.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 
 public class Duke {
-    public static void main(String[] args) throws UnsupportedEncodingException, DukeException {
+    public static void main(String[] args) throws UnsupportedEncodingException {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -12,6 +12,7 @@ public class Duke {
         String line = "____________________________________________________________";
         ArrayList<Task> tasks = new ArrayList<Task>();
         Scanner sc = new Scanner(System.in);
+        PrintStream printStream = new PrintStream(System.out, true, "UTF-8");;
 
         //Greeting
         System.out.println(line + "\nHello! I'm Duke\nWhat can I do for you?\n" + line);
@@ -26,99 +27,34 @@ public class Duke {
             } else if (command.equals("list")) {
                 System.out.println(line + "\nHere are the tasks in your list: ");
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println(i + 1 + ". " + tasks.get(i));
+                    printStream.println(i + 1 + ". " + tasks.get(i));
                 }
                 System.out.println(line);
             } else {
-                if (command.equals("delete")) {
-                    int index = sc.nextInt();
-                    try {
-                        if (index <= tasks.size()) {
-                            Task delTask = tasks.get(index - 1);
-                            tasks.remove(index - 1);
-                            System.out.println(line + "\nNoted. I've removed this task:  \n" + delTask + "\n"
-                                    + "Now you have " + tasks.size() + " tasks in the list.\n" + line);
-                        } else {
-                            throw new DukeException("Invalid Delete Description");
-                        }
-                    } catch (DukeException ex) {
-                        System.out.println("____________________________________________________________\n" +
-                                "OOPS!!! The task is unavailable.\n" +
-                                "____________________________________________________________");
-                    }
-                } else if (command.equals("done")) {
-                    int index = sc.nextInt();
-                    try {
-                        if (index <= tasks.size()) {
-                            Task doneTask = tasks.get(index - 1);
-                            doneTask.done();
-                            System.out.println(line + "\nNice! I've marked this task as done: \n" + doneTask + "\n" + line);
-                        } else {
-                            throw new DukeException("Invalid Done Description");
-                        }
-                    } catch (DukeException ex) {
-                        System.out.println("____________________________________________________________\n" +
-                                "OOPS!!! The task is unavailable.\n" +
-                                "____________________________________________________________");
-                    }
+                if (command.equals("done")) {
+                    Task doneTask = tasks.get(sc.nextInt() - 1);
+                    doneTask.done();
+                    printStream.println(line + "\nNice! I've marked this task as done: \n" + doneTask + "\n" + line);
                 } else if (command.equals("todo")) {
                     String input = sc.nextLine();
-                    try {
-                        if (!input.isEmpty()) {
-                            Todo newTask = new Todo(input);
-                            tasks.add(newTask);
-                            System.out.println(line + "\n" + "Got it. I've added this task: \n" + newTask
-                                    + "\nNow you have " + tasks.size() + " tasks in the list.\n" + line);
-                        } else {
-                            throw new DukeException("Invalid Todo Description");
-                        }
-                    } catch (DukeException ex) {
-                        System.out.println("____________________________________________________________\n" +
-                                "OOPS!!! The description of a todo cannot be empty.\n" +
-                                "____________________________________________________________");
-                    }
+                    Todo newTask = new Todo(input);
+                    tasks.add(newTask);
+                    System.out.println(line + "\n" + "Got it. I've added this task: \n" + newTask
+                            + "\nNow you have " + tasks.size() + " tasks in the list.\n" + line);
                 } else if (command.equals("deadline")) {
                     String input = sc.nextLine();
-                    try {
-                        if (!input.isEmpty()) {
-                            String[] splitTime = input.split(" /by ");
-                            Deadline newTask = new Deadline(splitTime[0], splitTime[1]);
-                            tasks.add(newTask);
-                            System.out.println(line + "\n" + "Got it. I've added this task: \n" + newTask
-                                    + "\nNow you have " + tasks.size() + " tasks in the list.\n" + line);
-                        } else {
-                            throw new DukeException("Invalid Deadline Description");
-                        }
-                    } catch (DukeException ex) {
-                        System.out.println("____________________________________________________________\n" +
-                                "OOPS!!! The description of a deadline cannot be empty.\n" +
-                                "____________________________________________________________");
-                    }
+                    String[] splitTime = input.split(" /by ");
+                    Deadline newTask = new Deadline(splitTime[0], splitTime[1]);
+                    tasks.add(newTask);
+                    System.out.println(line + "\n" + "Got it. I've added this task: \n" + newTask
+                            + "\nNow you have " + tasks.size() + " tasks in the list.\n" + line);
                 } else if (command.equals("event")) {
                     String input = sc.nextLine();
-                    try {
-                        if (!input.isEmpty()) {
-                            String[] splitTime = input.split(" /at ");
-                            Event newTask = new Event(splitTime[0], splitTime[1]);
-                            tasks.add(newTask);
-                            System.out.println(line + "\n" + "Got it. I've added this task: \n" + newTask
-                                    + "\nNow you have " + tasks.size() + " tasks in the list.\n" + line);
-                        } else {
-                            throw new DukeException("Invalid Event Description");
-                        }
-                    } catch (DukeException ex) {
-                        System.out.println("____________________________________________________________\n" +
-                                "OOPS!!! The description of a event cannot be empty.\n" +
-                                "____________________________________________________________");
-                    }
-                } else {
-                    try {
-                        throw new DukeException("Invalid Command");
-                    } catch (DukeException ex) {
-                        System.out.println("____________________________________________________________\n" +
-                                "OOPS!!! I'm sorry, but I don't know what that means :-(\n" +
-                                "____________________________________________________________");
-                    }
+                    String[] splitTime = input.split(" /at ");
+                    Event newTask = new Event(splitTime[0], splitTime[1]);
+                    tasks.add(newTask);
+                    System.out.println(line + "\n" + "Got it. I've added this task: \n" + newTask
+                            + "\nNow you have " + tasks.size() + " tasks in the list.\n" + line);
                 }
             }
         }
