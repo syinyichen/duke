815b160dfac3c5852fa3394e28747d0a68447ab5
syinyichen <e0313651@u.nus.edu>
2020-02-01 13:21:44 +0800

    Level-10

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -2,54 +2,35 @@ package duke;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import duke.Ui;
-import duke.Parser;
-import duke.TaskList;
-import duke.Storage;
-import duke.DukeException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * <code>Duke</code> is a chatbox that keep tracks of various tasks.
  */
-public class Duke {
+public class Duke extends Application {
     private Ui ui;
     private Parser parser;
     private TaskList taskList;
     private Storage storage;
 
-    /**
-     * Launch a new <code>UI</code> to interact with user.
-     * Indicates the <code>filePath</code> to store tasks.
-     *
-     * @throws FileNotFoundException  If file is not found in the given <code>filePath</code>.
-     */
-    public Duke(String filePath) throws FileNotFoundException {
-        ui = new Ui();
-        storage = new Storage(filePath);
+    public Duke() {
+
+    }
+
+    @Override
+    public void start(Stage stage) {
         try {
-            taskList = new TaskList(storage.load(), ui);
-        } catch (DukeException e) {
-            ui.showLoadingError();
-            taskList = new TaskList(ui);
+            FXMLLoader fxmlLoader = new FXMLLoader(Duke.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = (AnchorPane) fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    }
-
-    /**
-     * Chatbox starts by greeting user.
-     * <code>Parser</code> is used to scan user input.
-     * Various tasks are carried out based on the user input.
-     * Tasks are stored in a file.
-     *
-     * @throws IOException  If file is unavailable for writing in <code>Storage</code>
-     */
-    public void run() throws IOException {
-        ui.greet();
-        parser = new Parser(ui, taskList);
-        parser.run();
-        storage.writeStorage(taskList);
-    }
-
-    public static void main(String[] args) throws IOException {
-        new Duke("C:/Users/syiny/Desktop/Study/CS2103T/duke/data/duke.txt").run();
     }
 }
